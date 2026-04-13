c04aa9b32e87d8f4bfca7bf26c619a5e06a151d1
1709Meg <meghickey@yahoo.com>
2026-04-13 19:47:45 -0230

    checkout page logic

--- final-sprint/src/components/CartItem.jsx
+++ final-sprint/src/components/CartItem.jsx
@@ -1,23 +1,33 @@
 import "./CartItem.css";
 
-const CartItem = ({ item, increaseQty, decreaseQty, removeFromCart }) => {
+const CartItem = ({
+  item,
+  increaseQty,
+  decreaseQty,
+  removeFromCart,
+  variant,
+}) => {
+  const isCheckout = variant === "checkout";
   return (
-    <div className="cart-item">
+    <div className={`cart-item ${isCheckout ? "checkout-item" : ""}`}>
       <img src="https://placehold.co/400x400" alt="cart item image" />
       <div className="cart-details">
         <p>{item.name}</p>
         <p>${item.price}.00</p>
-        <p>{item.description}</p>
+        {isCheckout && <p>Qty: {item.quantity}</p>}
+        {!isCheckout && <p>{item.description}</p>}
       </div>
-      <div className="cart-controls">
-        <div className="quantity-controls">
-          <button onClick={() => decreaseQty(item)}>-</button>
-          <span>{item.quantity}</span>
-          <button onClick={() => increaseQty(item)}>+</button>
-        </div>
+      {!isCheckout && (
+        <div className="cart-controls">
+          <div className="quantity-controls">
+            <button onClick={() => decreaseQty(item)}>-</button>
+            <span>{item.quantity}</span>
+            <button onClick={() => increaseQty(item)}>+</button>
+          </div>
 
-        <button onClick={() => removeFromCart(item)}>remove</button>
-      </div>
+          <button onClick={() => removeFromCart(item)}>remove</button>
+        </div>
+      )}
     </div>
   );
 };
--- final-sprint/src/pages/Checkout.jsx
+++ final-sprint/src/pages/Checkout.jsx
@@ -1,5 +1,61 @@
-const Checkout = () => {
-  return <div></div>;
+import "./Checkout.css";
+import OrderSummary from "../components/OrderSummary";
+import { useState } from "react";
+
+const Checkout = ({ cart }) => {
+  const [success, setSuccess] = useState(false);
+
+  // Stops page refresh
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSuccess(true);
+  };
+  return (
+    <div className="checkout-page">
+      <div className="checkout-container">
+        {success && (
+          <div className="success-box">
+            <p>Order placed successfully!</p>
+          </div>
+        )}
+        <form className="checkout-form" onSubmit={handleSubmit}>
+          <h1>Checkout</h1>
+          <section>
+            <h2>Contact Info</h2>
+            <input type="text" placeholder="Full Name" />
+            <input type="email" placeholder="Email" />
+          </section>
+
+          <section>
+            <h2>Shipping Address</h2>
+            <input type="text" placeholder="Address" />
+            <input type="text" placeholder="City" />
+
+            <div className="row">
+              <input type="text" placeholder="Province" />
+              <input type="text" placeholder="Postal Code" />
+            </div>
+          </section>
+
+          <section>
+            <h2>Payment</h2>
+            <input type="text" placeholder="Card Number" />
+
+            <div className="row">
+              <input type="text" placeholder="MM/YY" />
+              <input type="text" placeholder="CVC" />
+            </div>
+          </section>
+
+          <button type="submit">Place Order</button>
+        </form>
+
+        <div className="order-summary">
+          <OrderSummary cart={cart} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
