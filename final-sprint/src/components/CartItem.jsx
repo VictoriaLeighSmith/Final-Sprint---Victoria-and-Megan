1f5553ad4b289ee2998fab37ac1088647b2530dc
1709Meg <meghickey@yahoo.com>
2026-04-13 16:27:20 -0230

    cart page logic

--- final-sprint/src/components/CartItem.jsx
+++ final-sprint/src/components/CartItem.jsx
@@ -1,5 +1,25 @@
-const CartItem = () => {
-  return <div></div>;
+import "./CartItem.css";
+
+const CartItem = ({ item, increaseQty, decreaseQty, removeFromCart }) => {
+  return (
+    <div className="cart-item">
+      <img src="https://placehold.co/400x400" alt="cart item image" />
+      <div className="cart-details">
+        <p>{item.name}</p>
+        <p>${item.price}.00</p>
+        <p>{item.description}</p>
+      </div>
+      <div className="cart-controls">
+        <div className="quantity-controls">
+          <button onClick={() => decreaseQty(item)}>-</button>
+          <span>{item.quantity}</span>
+          <button onClick={() => increaseQty(item)}>+</button>
+        </div>
+
+        <button onClick={() => removeFromCart(item)}>remove</button>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
