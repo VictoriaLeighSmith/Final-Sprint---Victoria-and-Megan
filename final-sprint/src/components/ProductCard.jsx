b287757abffbd54a27abb742624140a8108e991f
1709Meg <meghickey@yahoo.com>
2026-04-15 13:40:19 -0230

    merge main + add checkout page

--- final-sprint/src/components/ProductCard.jsx
+++ final-sprint/src/components/ProductCard.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import "./ProductCard.css";
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, compact = false }) => {
   return (
-    <div className="product-container">
+    <div className={`product-container ${compact ? "compact-card" : ""}`}>
       <Link to={`/products/${product.id}`} className="product-card-link">
         <div className="product-image-container">
           <img src={product.image} alt={product.name}></img>
@@ -13,7 +13,9 @@ const ProductCard = ({ product }) => {
           <h3>{product.name}</h3>
           <p>${product.price.toFixed(2)}</p>
 
-          <p>{product.description}</p>
+          <br />
+
+          {!compact && <p>{product.description}</p>}
         </div>
       </Link>
     </div>
