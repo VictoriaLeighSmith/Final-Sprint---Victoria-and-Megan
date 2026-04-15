7fc8b7fd42da45fc9e0cf0f76105078b30e035eb
VictoriaLeighSmith <victoria.smith1@keyin.com>
2026-04-15 17:32:38 -0230

    home-layout

--- final-sprint/src/pages/Home.jsx
+++ final-sprint/src/pages/Home.jsx
@@ -18,11 +18,13 @@ const Home = ({ products }) => {
 
       <div className="home-page-section-1">
         <div className="section-1-image-container">
-          <img src="https://placehold.co/400x500" />
+          <img src="https://placehold.co/600x600" />
         </div>
 
         <div className="section-1-info-container">
-          <p>Makeup</p>
+          <h3 className="section-1-title">
+            <em>Makeup</em>
+          </h3>
 
           <div className="home-product-preview-row">
             {sectionOneProducts.map((product) => (
@@ -34,7 +36,9 @@ const Home = ({ products }) => {
 
       <div className="home-page-section-2">
         <div className="section-2-info-container">
-          <p>Sets</p>
+          <h3 className="section-2-title">
+            <em>Sets</em>
+          </h3>
 
           <div className="home-product-preview-row">
             {sectionTwoProducts.map((product) => (
@@ -44,7 +48,7 @@ const Home = ({ products }) => {
         </div>
 
         <div className="section-2-image-container">
-          <img src="https://placehold.co/400x500" />
+          <img src="https://placehold.co/600x600" />
         </div>
       </div>
     </div>
